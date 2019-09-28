8dbfcf6e4a71bced46c7d598a6aaa675dcf0b9fc
sato-kai <h175bkznpicloud.com>
2019-09-28 11:04:48 +0900

    delete binding.pry

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    # binding.pry
     group = Group.find(params[:group_id])
     respond_to do |format|
       format.json{ @messages = group.messages.includes(:user).where("id > ?", params[:id])}
